03b8d6a93c26d0e5e2a4dee5d1983a5d40e70263
vfornerprofesor <vforner.profesor@gmail.com>
2023-05-23 12:59:20 +0200

    Microbit

--- scripts/load_blocks.js
+++ scripts/load_blocks.js
@@ -1,27 +1,34 @@
 function loadBlocks(page, element) {
     let blocks = pages[page];
 
-    for(let i = 0; i < blocks.length; i++) {
+    for (let i = 0; i < blocks.length; i++) {
         console.log(blocks[i]);
         createBlock(blocks[i], element);
     }
 }
 
 function createBlock(block, element) {
-    if(block[0] == 'block_h1') {
-        createBlockH1(block, element);
-    } else {
-        if(block[0] == 'block_h2') {
+    switch (block[0]) {
+        case 'block_h1':
+            createBlockH1(block, element);
+            break;
+        case 'block_h2':
             createBlockH2(block, element);
-        } else {
-            if(block[0] == 'block_h3') {
-                createBlockH3(block, element);
-            } else {
-                if(block[0] == 'block_unit') {
-                    createBlockUnit(block, element);
-                }
-            }
-        }
+            break;
+        case 'block_h3':
+            createBlockH3(block, element);
+            break;
+        case 'block_h4':
+            //createBlockH1(block, element);
+            break;
+        case 'block_unit':
+            createBlockUnit(block, element);
+            break;
+        case 'block_content':
+            createBlockContent(block, element);
+            break;
+        default:
+            break;
     }
 }
 
@@ -33,7 +40,7 @@ function createBlockH1(block, element) {
     th1.textContent = block[1];
 
     div.appendChild(th1);
-    
+
     var el = document.getElementById(element);
     el.appendChild(div);
 }
@@ -45,7 +52,7 @@ function createBlockH2(block, element) {
     th2.textContent = block[1];
 
     div.appendChild(th2);
-    
+
     var el = document.getElementById(element);
     el.appendChild(div);
 }
@@ -57,45 +64,45 @@ function createBlockH3(block, element) {
     th3.textContent = block[1];
 
     div.appendChild(th3);
-    
+
     var el = document.getElementById(element);
     el.appendChild(div);
 }
 
 function createBlockUnit(block, element) {
-    
+
     var div_b = document.createElement('div');
     div_b.classList.add('block');
 
     var div_r = document.createElement('div');
     div_r.classList.add('row');
 
-    for(let i = 1; i < block.length; i++) {
+    for (let i = 1; i < block.length; i++) {
         var url = urls[block[i][0]];
         var img_src = imgs[block[i][1]];
         var name = block[i][2];
 
         var div_c = document.createElement('div');
         div_c.classList.add('col');
-    
+
         var a = document.createElement('a');
         a.classList.add('unit-link');
         a.href = url;
-    
+
         var div_u = document.createElement('div');
         div_u.classList.add('unit');
-    
+
         var img = document.createElement('img');
         img.classList.add('unit-img');
         img.src = img_src;
-    
+
         var th3 = document.createElement('h3');
         th3.classList.add('btn');
         th3.classList.add('btn-primary');
         th3.classList.add('unit-title');
         th3.textContent = name;
-    
-    
+
+
         div_u.appendChild(img);
         div_u.appendChild(th3);
         a.appendChild(div_u);
@@ -107,4 +114,105 @@ function createBlockUnit(block, element) {
 
     var el = document.getElementById(element);
     el.appendChild(div_b);
+}
+
+function createBlockContent(block, element) {
+    var el = document.getElementById(element);
+    var div = document.createElement('div');
+    div.classList.add('block');
+    for (let i = 1; i < block.length; i++) {
+        var new_element = createElementByName(block[i][0], block[i]);
+        div.appendChild(new_element);
+    }
+    el.appendChild(div);
+}
+
+function createElementByName(name, block) {
+    var new_element; 
+    switch (name) {
+        case 'text_complex':
+            new_element = createElementTextComplex(block);
+            break;
+        case 'text':
+            new_element = createElementText(block);
+            break;
+        case 'img_center':
+            new_element = createElementImg(block);
+            break;
+        case 'cols':
+            new_element = createElementCols(block);
+            break;
+        case 'list_group':
+            new_element = createElementListGroup(block);
+            break;
+        default:
+            break;
+    }
+    return new_element;
+}
+
+function createElementListGroup(block) {
+    var ul = document.createElement('ul');
+    ul.classList.add('list-group');
+    for (var i = 0; i < block[1].length; i++) {
+        var li = document.createElement('li');
+        li.classList.add('list-group-item');
+        //var new_element = createElementByName(block[1][i][0], block[1][i]);
+        li.textContent = block[1][i][1];
+        //li.appendChild(new_element);
+        ul.appendChild(li);
+    }
+    return ul;
+}
+
+
+function createElementCols(block) {
+    debugger;
+    var row = document.createElement('div');
+    row.classList.add('row');
+
+    for (var i = 1; i < block.length; i++) {
+        var col = document.createElement('div');
+        col.classList.add('col');
+
+        for (var j = 0; j < block[i].length; j++) {
+            var new_element = createElementByName(block[i][j][0], block[i][j]);
+            
+            col.appendChild(new_element);
+        }
+        row.appendChild(col);
+    }
+    return row;
+}
+
+function createElementImg(block) {
+    var img = document.createElement('img');
+    img.classList.add('img-fluid');
+    img.classList.add('my-3');
+    img.classList.add('center');
+    img.src = block[1];
+    return img;
+}
+
+function createElementText(block) {
+    var p = document.createElement('p');
+    p.innerHTML = block[1];
+    return p;
+}
+
+function createElementTextComplex(block) {
+    var p = document.createElement('p');
+    p.innerHTML = '';
+    for (let j = 1; j < block.length; j++) {
+        switch (block[j][0]) {
+            case 'text':
+                p.innerHTML += ' ' + block[j][1];
+                break;
+            case 'link':
+                p.innerHTML += ' <a href="' + block[j][1] + '">' + block[j][2] + '</a>';
+            default:
+                break;
+        }
+    }
+    return p;
 }
--- scripts/pages.js
+++ scripts/pages.js
@@ -24,5 +24,55 @@ const pages = {
         ['block_unit', ['inteligencia_artificial', 'inteligencia_artificial', 'Conceptes bàsics IA'], ['inteligencia_artificial', 'inteligencia_artificial', 'Ferramentes IA']],
         ['block_h2', 'OFIMÀTICA'],
         ['block_unit', ['google_calcul', 'google_calcul', 'Full de càlcul']],
+    ],
+    'microbit': [
+        ['block_content', [
+            'text_complex',
+                ['text', 'Curs extret del'],
+                ['link', 'https://sites.google.com/site/migueltecnologia/1%C2%BA-eso/computaci%C3%B3n-y-rob%C3%B3tica-1/tema-4-rob%C3%B3tica-con-maqueen', 'material'],
+                ['text', "de Miguel Tecnologia de l'IES José Saramargo"]
+            ]
+        ],
+        ['block_h2', 'Què és?'],
+        ['block_content', [
+            'text', "La tarjeta microbit és una placa que es pot programar perquè execute instruccions i realitze tasques. A més, es pot connectar al robot Maqueen per tal d'ampliar funcionalitat i donar-li mobilitat",
+            ],[
+            'img_center', 'img/microbit.jpg'  
+            ], [
+            'text', "A continuació podrem veure tots els sensors i actuadors que té el robot:"
+            ],[
+            'img_center', 'img/robotica/microbit/maqueen.png'  
+            ]
+        ],
+        ['block_h2', 'Motors'],
+        ['block_content', [
+            'text_complex',
+                ['text', "Per començar a programar hem d'accedir a"],
+                ['link', 'https://makecode.microbit.org/', 'Makecode'],
+            ], [
+            'text', "Allí afegirem l'extensió Maqueen des de l'apartat Extensions, on buscarem Maqueen"
+            ], [
+            'cols',
+                [['img_center', "img/robotica/microbit/01.png"]],
+                [['img_center', "img/robotica/microbit/02.png"]]
+            ], [
+            'text', "Des d'aleshores tindrem un apartat anomenat Maqueen que tindrà tots els blocs per controlar el robot, tant per rebre informació dels sensors com per enviar-li informació perquè realitze alguna cosa."
+            ],[
+            'img_center', 'img/robotica/microbit/03.png'  
+            ], [
+            'text', "El bloc motor ens permet moure o un dels dos motors o els dos a la vegada. A més ens indica si volem avançar o tirar enrere i la velocitat a la que ho volem fer. La velocitat pot tindre un valor entre 0 i 255."
+            ], [
+            'text', "A més, el bloc Parar ens permetrà para un o els dos motors del robot."
+            ], [
+            'text', "Amb tot açò podem fer diferents accions:"
+            ], [
+            'list_group', [
+                ['text', "Moure avant: posar els dos motors en marxa"],
+                ['text', "Tirar enrere: posar els dos motors en marxa cap enrere"],
+                ['text', "Parar: posar els dos motors en pausa"],
+                ['text', "Girar: podem posar només un motor en marxa o un en marxa cap endavant i l'altre cap enrere"]
+                ]
+            ]
+        ],
     ]
 };
